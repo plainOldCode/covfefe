5445f2cfe3f1365f2f52a77e6ae14bc6c89a2d40
skshim in windows 10 <nodelay@gmail.com>
2017-06-01 22:11:58 +0900

    Use express

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 let msg = 'covfefe';
-console.log(msg);
+let count = 0;
+const getTick = ()=>count++;
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+	response.json({ msg : msg, tick : getTick()});
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
