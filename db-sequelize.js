21d80b682b2dbc9308a871a0c68215b43f9b5958
skshim in windows 10 <nodelay@gmail.com>
2017-06-05 23:22:49 +0900

    Add sequelize package

---
+++ db-sequelize.js
@@ -0,0 +1,46 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+	host: 'localhost',
+	dialect: 'sqlite',
+	pool: {
+		max: 5,
+		min: 0,
+		idle: 10000
+	},
+	// SQLite only
+	storage: ':memory:'
+	//storage: './temp.db'
+});
+
+sequelize
+	.authenticate()
+	.then(() => {
+		console.log('Connection has been established successfully.');
+	})
+	.catch(err => {
+		console.error('Unable to connect to the database:', err);
+	});
+
+const User = sequelize.define('user', {
+  firstName: {
+    type: Sequelize.STRING
+  },
+  lastName: {
+    type: Sequelize.STRING
+  }
+});
+
+// force: true will drop the table if it already exists
+User.sync({force: true}).then(() => {
+  // Table created
+  return User.create({
+    firstName: 'John',
+    lastName: 'Hancock'
+  });
+}).then(()=>{
+	return User.findAll().then(users => {
+		console.log(users)
+	});
+});
+
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 
 describe("Test code start",function() {
-		it("Test code run",function() {
-		});
+	it("Test code run",function() {
+	});
 });
