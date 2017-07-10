562aa2d5aa3a1ccec3a5fbc17b7d872b039c7fa1
skshim in windows 10 <nodelay@gmail.com>
2017-07-10 17:04:49 +0900

    Add image to each class

--- src/lib/game/main.js
+++ src/lib/game/main.js
@@ -3,9 +3,150 @@ ig.module(
 )
 .requires(
 	'impact.game',
-	'impact.font'
+	'impact.font',
+	'impact.entity'
 )
 .defines(function(){
+
+iPowerEffect = ig.Entity.extend({
+	size : { x : 16, y: 16 },
+	offset : { x : 0, y: 0 },
+	animSheet : new ig.AnimationSheet('media/powerimage.png',16,16),
+
+	init : function (x,y,settings) {
+		this.addAnim('idle',0.1,[0]);
+		this.parent(x,y,settings);
+	},
+
+	update : function () {
+		//this.parent();
+	}
+});
+
+iPowerGenerator = ig.Entity.extend({
+	size : { x : 32, y: 32 },
+	offset : { x : 0, y: 0 },
+	animSheet : new ig.AnimationSheet('media/powergenerator.png',32,32),
+
+	init : function (x,y,settings) {
+		this.addAnim('idle',0.1,[0]);
+		this.parent(x,y,settings);
+		this.count = 0;
+		this.effect = null;
+	},
+
+	addEffect : function (entityGenerator) {
+		this.entityGenerator = entityGenerator;
+	},
+
+	update : function () {
+		this.parent();
+		this.count++;
+		if ( this.count == 120 ) {
+			this.count = 0;
+			if (!this.effect) {
+				this.effect = this.entityGenerator();
+			} else {
+				this.effect.kill();
+				this.effect = null; 
+			}
+		}
+	}
+});
+
+iCoinEffect = ig.Entity.extend({
+	size : { x : 16, y: 16 },
+	offset : { x : 0, y: 0 },
+	animSheet : new ig.AnimationSheet('media/coinimage.png',16,16),
+
+	init : function (x,y,settings) {
+		this.addAnim('idle',0.1,[0]);
+		this.parent(x,y,settings);
+	},
+
+	update : function () {
+		this.parent();
+	}
+});
+
+iGPU = ig.Entity.extend({
+
+	size : { x : 32, y: 32 },
+	offset : { x : 0, y: 0 },
+	animSheet : new ig.AnimationSheet('media/gpu.png',32,32),
+
+	init : function (x,y,settings) {
+		this.addAnim('idle',0.1,[0]);
+		this.parent(x,y,settings);
+		this.count = 0;
+		this.effect = null;
+	},
+
+	addEffect : function (entityGenerator) {
+		this.entityGenerator = entityGenerator;
+	},
+
+	update : function () {
+		this.parent();
+		this.count++;
+		if ( this.count == 120 ) {
+			this.count = 0;
+			if (!this.effect) {
+				this.effect = this.entityGenerator();
+			} else {
+				this.effect.kill();
+				this.effect = null; 
+			}
+		}
+	}
+});
+
+
+iCoin = ig.Entity.extend({
+	size : { x : 32, y: 32 },
+	offset : { x : 0, y: 0 },
+	animSheet : new ig.AnimationSheet('media/coin.png',32,32),
+
+	init : function (x,y,settings) {
+		this.addAnim('idle',0.1,[0]);
+		this.parent(x,y,settings);
+	},
+
+	update : function () {
+		this.parent();
+	}
+});
+
+
+iRealMoney = ig.Entity.extend({
+	size : { x : 32, y: 32 },
+	offset : { x : 0, y: 0 },
+	animSheet : new ig.AnimationSheet('media/realmoney.png',32,32),
+
+	init : function (x,y,settings) {
+		this.addAnim('idle',0.1,[0]);
+		this.parent(x,y,settings);
+	},
+
+	update : function () {
+		this.parent();
+	}
+});
+
+iTradeMarket = ig.Entity.extend({
+	size : { x : 32, y: 32 },
+	offset : { x : 0, y: 0 },
+	animSheet : new ig.AnimationSheet('media/trademarket.png',32,32),
+
+	init : function (x,y,settings) {
+		this.addAnim('idle',0.1,[0]);
+		this.parent(x,y,settings);
+	},
+
+	update : function () {
+		this.parent();
+	}
+});
 
 MyGame = ig.Game.extend({
 	
@@ -15,6 +156,13 @@ MyGame = ig.Game.extend({
 	
 	init: function() {
 		// Initialize your game here; bind keys etc.
+		this.powergenerator = this.spawnEntity(iPowerGenerator,ig.system.width/2-32,ig.system.height/2);
+		this.powergenerator.addEffect(()=>this.spawnEntity(iPowerEffect,ig.system.width/2-32-32,ig.system.height/2));
+		this.gpu = this.spawnEntity(iGPU,ig.system.width/2,ig.system.height/2);
+		this.gpu.addEffect(()=> this.spawnEntity(iCoinEffect,ig.system.width/2+32,ig.system.height/2+32));
+		this.coin = this.spawnEntity(iCoin,ig.system.width/2+32,ig.system.height/2);
+		this.realmoney = this.spawnEntity(iRealMoney,ig.system.width/2-32,ig.system.height/2+32);
+		this.trademarket = this.spawnEntity(iTradeMarket,ig.system.width/2,ig.system.height/2+32);
 	},
 	
 	update: function() {
